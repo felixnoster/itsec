a488b6ec733eef21f1d06fb5a9eb4ec4c2558477
felixnoster <58666226+felixnoster@users.noreply.github.com>
2022-06-09 19:02:17 +0200

    Start

---
+++ client/src/main/java/edu/hm/cs/ib/itsec/client/Client.java
@@ -0,0 +1,244 @@
+package edu.hm.cs.ib.itsec.client;
+
+import org.bouncycastle.asn1.x9.X9ECParameters;
+import org.bouncycastle.crypto.ec.CustomNamedCurves;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+import org.bouncycastle.pqc.math.linearalgebra.ByteUtils;
+import org.bouncycastle.util.encoders.Base64;
+
+import javax.crypto.SecretKey;
+import java.io.*;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+
+
+
+public class Client {
+
+    private static final int TAG_LENGTH_BIT = 128;
+
+    private static final String EMAIL = "ilie.doni@hm.edu";
+
+    private static final String PROVIDER = "BC";
+    private Socket socket;
+    private BufferedReader bufIn;
+    private PrintWriter out;
+
+    private KeyPairGenerator keyGen;
+    private KeyPair pair;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public static void main(final String[] args) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException {
+        Security.setProperty("crypto.policy", "unlimited");
+        Security.insertProviderAt(new BouncyCastleProvider(), 1);
+        final Client c = new Client();
+        generateKeyPair();
+        c.run();
+    }
+
+    public static byte[] xorByteArray(byte[] input, int seqNumber) {
+        byte[] param = ByteBuffer.allocate(input.length).putInt(8, seqNumber).array();
+        return ByteUtils.xor(input, param);
+    }
+
+    private void run() {
+        try {
+            connect();
+            final var asymmetricResponse = doAsymmetricEncryption();
+            final var secret = doSymmetricEncryption(asymmetricResponse);
+            System.out.printf("Challenge complete! Your personal secret: %s %n", secret);
+            disconnect();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Connects the client to the server component.
+     *
+     * @throws IOException may throw an IOException e.g. if the host is unreachable.
+     */
+    private void connect() throws IOException {
+        socket = new Socket("10.28.250.70", 9000);
+        bufIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    /**
+     * Closes all connections and disconnects the client
+     *
+     * @throws IOException may throw an IOException e.g. if the client died already.
+     */
+    private void disconnect() throws IOException {
+        out.close();
+        bufIn.close();
+        socket.close();
+    }
+
+    /**
+     * This method provides the communication for the asymmetric encrypted data.
+     *
+     * @param encryptedPublicKey Your own encrypted public key.
+     * @param encryptedEmail     Your own encrypted email address.
+     * @return the encrypted shared key for the symmetric encryption and the encrypted iv. (Must be decrypted again!)
+     * @throws IOException May throw an IOException e.g. if the host is unreachable, reading data from the network failed or an invalid format.
+     */
+    private EncryptedAsymmetricResponse sendAsymmetricData(final byte[] encryptedPublicKey,
+                                                           final byte[] encryptedEmail) throws IOException {
+        out.println(Base64.toBase64String(encryptedEmail));
+        out.println(Base64.toBase64String(encryptedPublicKey));
+        EncryptedAsymmetricResponse response = new EncryptedAsymmetricResponse();
+        response.SharedSecret = Base64.decode(bufIn.readLine());
+        response.InitializationVector = Base64.decode(bufIn.readLine());
+        return response;
+    }
+
+    /**
+     * Sends the symmetric encrypted data to the server and returns your encrypted secret.
+     *
+     * @param encryptedEmail your encrypted E-Mail.
+     * @return An byte array containing the encrypted secret.
+     * @throws IOException May throw an IOException e.g. if the host is unreachable, reading data from the network failed or an invalid format.
+     */
+    private byte[] sendSymmetricData(final byte[] encryptedEmail) throws IOException {
+        out.println(Base64.toBase64String(encryptedEmail));
+        return Base64.decode(bufIn.readLine());
+    }
+
+    /**
+     * Get the provided public key of the server.
+     *
+     * @return the public key.
+     */
+    private PublicKey getPubkey() throws Exception {
+        final Path pubKey = Paths.get("data/public.key");
+        try (final BufferedReader pubKeyreader = Files.newBufferedReader(pubKey)) {
+            final KeyFactory kf = KeyFactory.getInstance("EC", PROVIDER);
+            final X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(Base64.decode(pubKeyreader.readLine()));
+            return kf.generatePublic(pubKeySpec);
+        }
+    }
+
+    private AsymmetricResponse doAsymmetricEncryption() throws Exception {
+        var returnValue = new AsymmetricResponse();
+
+        /**
+         * Your task is to implement this method.
+         * The following steps must be implemented: 
+         *  - Generate an keypair using the curve25519
+         *  - Encrypt the public component and your email using EC-IES with the public key of the server
+         *  - Send the data to the server and receive the encrypted IV and shared key
+         *  - Decrypt the data using EC-IES and your own private key and return the values
+         */
+
+        generateKeys("1024");
+        String method = "ECIES";
+
+        Cipher cipherObjectEncrypt = getEncryptCipherObject(getPubkey(), method); /*mit update methode die email rein mit ECIES und UTF-8*/
+
+
+        return returnValue;
+    }
+
+    public GenerateKeys(int keylength) throws NoSuchAlgorithmException, NoSuchProviderException {
+        this.keyGen = KeyPairGenerator.getInstance("EC", new BouncyCastleProvider());
+        this.keyGen.initialize(keylength);
+    }
+
+    public void createKeys() {
+        this.pair = this.keyGen.generateKeyPair();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+    }
+
+    public PrivateKey getPrivateKey() {
+        return this.privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return this.publicKey;
+    }
+
+    public void writeToFile(String path, byte[] key) throws IOException {
+        File f = new File(path);
+        f.getParentFile().mkdirs();
+
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(key);
+        fos.flush();
+        fos.close();
+    }
+
+
+    private static KeyPair generateKeyPair() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        X9ECParameters ecP = CustomNamedCurves.getByName("curve25519");
+        ECParameterSpec ecSpec = new ECParameterSpec(ecP.getCurve(), ecP.getG(), ecP.getN(), ecP.getH(), ecP.getSeed());
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("EC", new BouncyCastleProvider());
+        keyGen.initialize(ecSpec);
+        return keyGen.generateKeyPair();
+
+
+
+    }
+
+
+
+
+    private String doSymmetricEncryption(AsymmetricResponse asymmetricResponse) throws Exception {
+        String secret = null;
+
+        /**
+         * Your task is to implement this method.
+         * The following steps must be implemented: 
+         *  - Encrypt your email using AES-GCM, the provided shared key and IV
+         *  - Send the data to the server and receive the encrypted shared secret
+         *  - Decrypt the data using the shared key and IV
+         *  - The IV get's XOR'ed with the (virtual) message number starting at 1
+         *  - message number for communicating to server: 1
+         *  - message number for response to client: 2
+         */
+
+        return secret;
+    }
+}
+
+/**
+ * The (decrypted) content answered by the server containing the initial IV and the shared secret for the symmetric crypto.
+ */
+class AsymmetricResponse {
+
+    /**
+     * The initial IV. Must be XORed with the message number starting at 1.
+     */
+    public byte[] InitializationVector;
+
+    /**
+     * The secret key for the symmetric cryptography.
+     */
+    public SecretKey SharedSecret;
+
+}
+
+/**
+ * The encrypted content answered by the server containing the initial IV and the shared secret for the symmetric crypto.
+ */
+class EncryptedAsymmetricResponse {
+
+    /**
+     * The encrypted IV.
+     */
+    public byte[] InitializationVector;
+
+    /**
+     * The encrypted key.
+     */
+    public byte[] SharedSecret;
+
+}
